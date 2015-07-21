a3220c4fc585caa2b4d10f4d1a1649dd0fb3f486
Michael Elbaz <michael.elbaz25@gmail.com>
2015-07-21 15:33:45 -0400

    creating game states

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -22,7 +22,12 @@ class MainScene: CCNode {
     var gyroAvailable: Bool!
     let motionKit = MotionKit()
     weak var label: CCLabelTTF!
-
+    var timer: Double = 5.0
+    var gameState: String = "nothing" {
+        didSet {
+            
+        }
+    }
     
     func didLoadFromCCB () {
         var drop = CCBReader.load("Drop") as! Drop
@@ -34,16 +39,10 @@ class MainScene: CCNode {
 
         motionKit.getGyroValues(interval: 1.0){
             (gyroX, gyroY, gyroZ) in
-            //Do whatever you want with the x, y and z values
-            println("\(gyroX)","\(gyroY)","\(gyroZ)")
         }
         
-        motionKit.getAccelerometerValues(interval: 1.0){
-            (xAcc, yAcc, zAcc) in
-            
-            println("\(xAcc)","\(yAcc)","\(zAcc)")
-        }
-    
+        
+        schedule("shakeDetection", interval: 0.2)
 //        CCDirector.sharedDirector().view.addGestureRecognizer(s)
     
     }
@@ -55,16 +54,22 @@ class MainScene: CCNode {
     }
     
    override func update(delta: CCTime) {
-        
-//        println("Motion =")
+        timer -= 0.01
+    if timer <= 0 {
+        gameOver()
+    }
+    
+    
     }
 
+    func gameOver() {
+        println("game is over")
+    }
 
-    func randomAction() {
-            
-        var randomNumber = arc4random_uniform(4)
-
-    
+    func randomStateChange() {
+        
+          var randomNumber = arc4random_uniform(4)
+        
         drop.visible = false
         twist.visible = false
         lift.visible = false
@@ -72,19 +77,85 @@ class MainScene: CCNode {
         
         if randomNumber == 0 {
             drop.visible = true
+            gameState = "drop"
         } else if randomNumber == 1 {
             twist.visible = true
+             gameState = "twist"
         } else if randomNumber == 2 {
             lift.visible = true
+             gameState = "lift"
         } else if randomNumber == 3 {
             shake.visible = true
+             gameState = "shake"
+            schedule("shakeDetection", interval: 0.2)
+            
         }
-        
-        }//randomAction
+    }
     
+    func dropDetection() {
+        let threshold = 2.1;
         
+        motionKit.getAccelerometerValues(interval: 1.0){
+            (xAcc, yAcc, zAcc) in
+            
+            if xAcc > threshold || xAcc < -threshold ||
+                yAcc > threshold || yAcc < -threshold ||
+                zAcc > threshold || zAcc < -threshold {
+                    println("Drop Detected")
+            }
+        }
+    }
     
-    }//Class
+    func liftDetection() {
+        let threshold = 2.1;
+        
+        motionKit.getAccelerometerValues(interval: 1.0){
+            (xAcc, yAcc, zAcc) in
+            
+            if xAcc > threshold || xAcc < -threshold ||
+                yAcc > threshold || yAcc < -threshold ||
+                zAcc > threshold || zAcc < -threshold {
+                    println("Lift Detected")
+            }
+        }
+    }
+    
+    func twistDetection() {
+        let threshold = 2.1;
+        
+        motionKit.getGyroValues(interval: 1.0){
+            (gyroX, gyroY, gyroZ) in
+            
+            if gyroX > threshold || gyroX < -threshold ||
+                gyroY > threshold || gyroY < -threshold ||
+                gyroZ > threshold || gyroZ < -threshold {
+                    println("Twist Detected")
+            }
+        }
+    }
+    
+    
+    
+    
+    func shakeDetection() {
+       let threshold = 2.1;
+        
+        motionKit.getAccelerometerValues(interval: 1.0){
+            (xAcc, yAcc, zAcc) in
+            
+            if xAcc > threshold || xAcc < -threshold ||
+                yAcc > threshold || yAcc < -threshold ||
+                zAcc > threshold || zAcc < -threshold {
+                    println("Shake Detected")
+            }
+    
+        }
+
+    }
+    
+
+
+}//Class
 
 
 
